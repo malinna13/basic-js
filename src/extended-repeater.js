128405ef8c90a5bee9d3181f1a5fbab1b53cb124
Inna <malinna13@gmail.com>
2022-04-29 23:46:09 +0600

    complete task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,45 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const arr = [];
+  const addition = options.hasOwnProperty("addition")
+    ? String(options.addition)
+    : "";
+  const repeatTimes = options.repeatTimes || 1;
+  const additionRepeatTimes = options.additionRepeatTimes;
+  const separator = options.separator || "+";
+  const additionSeparator = options.additionSeparator || "|";
+
+  for (let i = 0; i < repeatTimes; i++) {
+    const innerArr = [];
+
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      innerArr.push(addition);
+    }
+
+    arr.push(
+      str + (additionRepeatTimes ? innerArr.join(additionSeparator) : addition)
+    );
+  }
+
+  return arr.join(separator);
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,39 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) return "Unable to determine the time of year!";
+  if (
+    Object.prototype.toString.call(date) !== "[object Date]" ||
+    Object.keys(date).length !== 0
+  ) {
+    throw new Error("Invalid date!");
+  }
+  try {
+    if (date.getMonth() === 11 || date.getMonth() <= 1) {
+      return "winter";
+    } else if (date.getMonth() >= 2 && date.getMonth() < 5) {
+      return "spring";
+    } else if (date.getMonth() >= 5 && date.getMonth() < 8) {
+      return "summer";
+    } else if (date.getMonth() >= 8 && date.getMonth() < 11) {
+      return "autumn";
+    }
+  } catch (error) {
+    throw new Error("Invalid date!");
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
